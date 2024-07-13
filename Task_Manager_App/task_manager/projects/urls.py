9c7e69dc59c871e5eadc2afba17b47294ac018e4
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-07-13 06:31:09 +0100

    nothing new though, trying to integrate it to the frontend

--- Task_Manager_App/task_manager/projects/urls.py
+++ Task_Manager_App/task_manager/projects/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
@@ -13,9 +13,9 @@ urlpatterns = [
     path('create/', views.project_create, name='project_create'),
     path('<int:pk>/update/', views.project_update, name='project_update'),
     path('<int:pk>/delete/', views.project_delete, name='project_delete'),
-    path('', views.index, name='index'),  # Example view
-    path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls')),
-    path('api/', include(router.urls)),
-    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
+    # path('', views.index, name='index'),  # Example view
+    # path('admin/', admin.site.urls),
+    # path('projects/', include('projects.urls')),
+    path('', include(router.urls)),
+    # path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
